abe3f151d2244dac0096975fc8e40423c8542911
Oliver Atkinson <oliveratkinson@me.com>
2016-04-02 10:13:23 +0100

    Test the assertion functions on Expectation

--- Tests/Source/Expectation_Spec.swift
+++ Tests/Source/Expectation_Spec.swift
@@ -10,6 +10,17 @@ import XCTest
 @testable import Expectation
 
 class Expectation_Spec: XCTestCase {
+  
+  override func tearDown() {
+    super.tearDown()
+    
+    ExpectationAssertFunctions.assertTrue   = ExpectationAssertFunctions.ExpectationAssertTrue
+    ExpectationAssertFunctions.assertFalse  = ExpectationAssertFunctions.ExpectationAssertFalse
+    ExpectationAssertFunctions.assertNil    = ExpectationAssertFunctions.ExpectationAssertNil
+    ExpectationAssertFunctions.assertNotNil = ExpectationAssertFunctions.ExpectationAssertNotNil
+    ExpectationAssertFunctions.fail         = ExpectationAssertFunctions.ExpectationFail
+    
+  }
 
   func testFunctions() {
     
@@ -18,6 +29,179 @@ class Expectation_Spec: XCTestCase {
     ExpectationAssertFunctions.ExpectationAssertNil(nil, "", file: #file, line: #line)
     ExpectationAssertFunctions.ExpectationAssertNotNil(NSObject(), "", file: #file, line: #line)
 
+  }
+  
+  func testFailure() {
+    
+    let failExpectation = expectationWithDescription("should call fail")
+    ExpectationAssertFunctions.fail = { message, file, line in
+      failExpectation.fulfill()
+    }
+    
+    fail()
+    
+    waitForExpectationsWithTimeout(1) {
+      XCTAssert($0 == nil)
+    }
+    
+  }
+  
+  struct A {
+    
+  }
+  
+  func testAssertTrue() {
+    
+    let expectation = expect(5)
+    
+    let assertTrue = expectationWithDescription("assertTrue is called")
+    ExpectationAssertFunctions.assertTrue = { expression, message, file, line in
+      assertTrue.fulfill()
+    }
+    
+    expectation.to
+    expectation.assertTrue(true, "")
+    
+    waitForExpectationsWithTimeout(1) {
+      XCTAssert($0 == nil)
+    }
+    
+  }
+  
+  func testAssertTrueInverse() {
+    
+    let expectation = expect(5)
+    
+    let assertFalse = expectationWithDescription("assertFalse is called")
+    ExpectationAssertFunctions.assertFalse = { expression, message, file, line in
+      assertFalse.fulfill()
+    }
+    
+    expectation.toNot
+    expectation.assertTrue(true, "")
+    
+    waitForExpectationsWithTimeout(1) {
+      XCTAssert($0 == nil)
+    }
+    
+  }
+  
+  func testAssertFalse() {
+    
+    let expectation = expect(5)
+    
+    let assertFalse = expectationWithDescription("assertFalse is called")
+    ExpectationAssertFunctions.assertFalse = { expression, message, file, line in
+      assertFalse.fulfill()
+    }
+    
+    expectation.to
+    expectation.assertFalse(true, "message")
+    
+    waitForExpectationsWithTimeout(1) {
+      XCTAssert($0 == nil)
+    }
+    
+  }
+  
+  func testAssertFalseInverse() {
+    
+    let expectation = expect(5)
+    
+    let assertTrue = expectationWithDescription("assertTrue is called")
+    ExpectationAssertFunctions.assertTrue = { expression, message, file, line in
+      assertTrue.fulfill()
+    }
+    
+    expectation.toNot
+    expectation.assertFalse(true, "")
+    
+    waitForExpectationsWithTimeout(1) {
+      XCTAssert($0 == nil)
+    }
+    
+  }
+  
+  func testAssertNil() {
+    
+    let expectation = expect(5)
+    
+    
+    let assertNil = expectationWithDescription("assertNil is called")
+    ExpectationAssertFunctions.assertNil = { expression, message, file, line in
+      assertNil.fulfill()
+    }
+    
+    let a: A? = A()
+    
+    expectation.to
+    expectation.assertNil(a, "")
+    
+    waitForExpectationsWithTimeout(1) {
+      XCTAssert($0 == nil)
+    }
+    
+  }
+  
+  func testAssertNilInverse() {
+    
+    let expectation = expect(5)
+    
+    let assertNotNil = expectationWithDescription("assertNotNil is called")
+    ExpectationAssertFunctions.assertNotNil = { expression, message, file, line in
+      assertNotNil.fulfill()
+    }
+    
+    let a: A? = A()
+    
+    expectation.toNot
+    expectation.assertNil(a, "")
+    
+    waitForExpectationsWithTimeout(1) {
+      XCTAssert($0 == nil)
+    }
+    
+  }
+  
+  func testAssertNotNil() {
+    
+    let expectation = expect(5)
+    
+    let assertNotNil = expectationWithDescription("assertNotNil is called")
+    ExpectationAssertFunctions.assertNotNil = { expression, message, file, line in
+      assertNotNil.fulfill()
+    }
+    
+    let a: A? = A()
+    
+    expectation.to
+    expectation.assertNotNil(a, "")
+    
+    waitForExpectationsWithTimeout(1) {
+      XCTAssert($0 == nil)
+    }
+    
+  }
+  
+  func testAssertNotNilInverse() {
+    
+    let expectation = expect(5)
+    
+    
+    let assertNil = expectationWithDescription("assertNil is called")
+    ExpectationAssertFunctions.assertNil = { expression, message, file, line in
+      assertNil.fulfill()
+    }
+    
+    let a: A? = A()
+    
+    expectation.toNot
+    expectation.assertNotNil(a, "")
+    
+    waitForExpectationsWithTimeout(1) {
+      XCTAssert($0 == nil)
+    }
+    
   }
 
 }
